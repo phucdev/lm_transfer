6b105196f8650563bd2308f96b98e406b483726f
Phuc Tran Truong <ptruong@outlook.de>
2024-08-16 14:38:38 +0200

    Update train_tokenizer.py
    - Clean up old configurations

--- src/train_tokenizer.py
+++ src/train_tokenizer.py
@@ -1,11 +1,10 @@
-from pathlib import Path
-
 import datasets
 import logging
 import argparse
 import requests
 import time
 
+from pathlib import Path
 from transformers import AutoTokenizer
 from rich.logging import RichHandler
 
@@ -17,13 +16,6 @@ logger.setLevel(logging.INFO)
 def parse_args():
     parser = argparse.ArgumentParser(description="Train a tokenizer")
     parser.add_argument(
-        "--use_combined_dataset",
-        action="store_true",
-        default=False,
-        help="Whether to use a combined dataset of BKAI news corpus, ViSoBERT social media text corpus, and Vietnamese"
-             "portion of OSCAR 2301, mC4 and Wikipedia",
-    )
-    parser.add_argument(
         "--dataset_name_or_path",
         type=str,
         help="Name of the dataset to train the tokenizer on",
@@ -34,10 +26,22 @@ def parse_args():
         help="Name of the dataset config to use",
     )
     parser.add_argument(
+        "--cache_dir",
+        type=str,
+        default=None,
+        help="Directory to cache the dataset",
+    )
+    parser.add_argument(
         "--output_dir",
         type=str,
         help="Output directory to save the tokenizer",
         required=True,
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=1000,
+        help="Batch size to use for training the tokenizer",
     )
     parser.add_argument(
         "--vocab_size",
@@ -55,20 +59,27 @@ def parse_args():
     return arguments
 
 
-def batch_iterator(data, batch_size=1000):
+def batch_iterator(data, batch_size=1000, max_retries=3, retry_delay=15):
+    # This function is used to iterate over the dataset when streaming
     batch = []
-    for dataset in data:
-        idx = 0
-        # TODO: try better retry logic
+    idx = 0
+    retries = 0
+
+    while retries <= max_retries:
         try:
-            for idx, example in enumerate(dataset):
+            for idx, example in enumerate(data):
                 batch.append(example["text"])
                 if len(batch) == batch_size:
                     yield batch
                     batch = []
+            break  # Exit the loop if successful
         except requests.exceptions.ConnectionError:
-            time.sleep(15)
-            for c_idx, example in enumerate(dataset):
+            retries += 1
+            if retries > max_retries:
+                raise
+            time.sleep(retry_delay)
+            # Continue from the last successful index
+            for c_idx, example in enumerate(data):
                 if c_idx < idx:
                     continue
                 batch.append(example["text"])
@@ -79,29 +90,40 @@ def batch_iterator(data, batch_size=1000):
         yield batch
 
 
+def get_training_corpus(data, batch_size=1000):
+    for start_idx in range(0, len(data), batch_size):
+        samples = data[start_idx:start_idx+batch_size]
+        yield samples["text"]
+
+
 def main():
     args = parse_args()
-    if args.use_combined_dataset:
-        logger.info("Using a combined dataset of BKAI news corpus, ViSoBERT social media text corpus, and Vietnamese"
-                    "portion of CulturaX and Wikipedia")
-        bkai_news = datasets.load_dataset("bkai-foundation-models/BKAINewsCorpus", split="train", streaming=True)
-        visobert = datasets.load_dataset("phucdev/ViSoBERT", split="train", streaming=True)
-        culturax = datasets.load_dataset("uonlp/CulturaX", "vi", split="train", streaming=True)
-        wikipedia = datasets.load_dataset("wikimedia/wikipedia", "20231101.vi", split="train", streaming=True)
-        data = [bkai_news, visobert, culturax, wikipedia]
-    elif Path(args.dataset_name_or_path).exists():
-        logger.info(f"Training tokenizer on {args.dataset_name_or_path} with vocab size {args.vocab_size}")
-        data = [datasets.load_dataset("json", data_files=args.dataset_name_or_path, split="train")]
+    logger.info(f"Training tokenizer on {args.dataset_name_or_path} with vocab size {args.vocab_size}")
+    # Local dataset
+    if Path(args.dataset_name_or_path).exists():
+        data = datasets.load_dataset("json", data_files=args.dataset_name_or_path, split="train")
+        training_corpus = get_training_corpus(data)
     else:
-        logger.info(f"Training tokenizer on {args.dataset_name_or_path} with vocab size {args.vocab_size}")
-        data = [datasets.load_dataset(
-            args.dataset_name_or_path, args.dataset_config_name, split="train", streaming=True
-        )]
+        # Load dataset from the Hugging Face hub
+        if args.cache_dir:
+            data = datasets.load_dataset(
+                args.dataset_name_or_path,
+                args.dataset_config_name,
+                split="train",
+                cache_dir=args.cache_dir
+            )
+            training_corpus = batch_iterator(data)
+        else:
+            # Stream the dataset (default)
+            data = datasets.load_dataset(
+                args.dataset_name_or_path, args.dataset_config_name, split="train", streaming=True
+            )
+            training_corpus = get_training_corpus(data)
 
     old_tokenizer = AutoTokenizer.from_pretrained(args.original_tokenizer)
     assert old_tokenizer.is_fast, "This script only works with fast tokenizers"
     tokenizer = old_tokenizer.train_new_from_iterator(
-        batch_iterator(data),
+        training_corpus,
         vocab_size=args.vocab_size,
         show_progress=True,
     )

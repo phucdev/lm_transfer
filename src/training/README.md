45a6c235076f1c95e17636ff9b91a759a62427c5
Phuc Tran Truong <ptruong@outlook.de>
2024-08-29 10:16:19 +0200

    Add training code

---
+++ src/training/README.md
@@ -0,0 +1,134 @@
+# Training
+This directory contains training scripts for:
+- Tokenizer training
+- Continued Pre-training of a Transformer model
+- Fine-tuning a Transformer model on a downstream task
+  - Token Classification
+  - Text Classification
+  - Question Answering
+
+## Tokenizer Training
+
+To train a tokenizer, you can use the `train_tokenizer.py` script. 
+The script takes in a dataset & an existing tokenizer and trains a new tokenizer model. 
+
+```bash
+python train_tokenizer.py \
+  --dataset_name_or_path=uonlp/CulturaX \
+    --dataset_config_name=vi \
+    --cache_dir=data/tokenizer_training \
+    --original_tokenizer=FacebookAI/xlm-roberta-base \
+    --vocab_size=50000 \
+    --output_dir=tokenizers/vi-spm-culturax \
+    --max_num_space_separated_tokens=16000
+```
+You can also use `max_num_bytes` to filter out examples that are too long for your purposes.
+I used this filter because long input sequences can cause issues for sentencepiece.
+As I am training a new tokenizer based on the XLM-RoBERTa tokenizer, which is based on sentencepiece with the 
+unigram language model segmentation algorithm, long sequences can cause overflow during the Expectation-Maximization 
+algorithm step.
+
+## Continued Pre-training
+
+
+## Fine-tuning
+The fine-tuning scripts are completely based on Hugging Face's `transformers` PyTorch examples: https://github.com/huggingface/transformers/tree/main/examples/pytorch
+
+### Token Classification
+For Named Entity Recognition (NER) or Part-of-Speech (POS) tagging, you can use the `run_ner.py` script.
+
+```bash
+python src/training/run_ner.py \
+  --model_name FacebookAI/xlm-roberta-base \
+  --dataset_name phucdev/PhoNER_COVID19 \
+  --dataset_config_name word \
+  --cache_dir /vol/tmp/truongph \
+  --output_dir=outputs/ner \
+  --do_train \
+  --do_eval \
+  --per_device_train_batch_size=8 \
+  --per_device_eval_batch_size=8 \
+  --num_train_epochs=3 \
+  --seed=42
+```
+
+The script expects the following format of the data:
+```json
+{
+    "chunk_tags": [11, 12, 12, 21, 13, 11, 11, 21, 13, 11, 12, 13, 11, 21, 22, 11, 12, 17, 11, 21, 17, 11, 12, 12, 21, 22, 22, 13, 11, 0],
+    "id": "0",
+    "ner_tags": [0, 3, 4, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "pos_tags": [12, 22, 22, 38, 15, 22, 28, 38, 15, 16, 21, 35, 24, 35, 37, 16, 21, 15, 24, 41, 15, 16, 21, 21, 20, 37, 40, 35, 21, 7],
+    "tokens": ["The", "European", "Commission", "said", "on", "Thursday", "it", "disagreed", "with", "German", "advice", "to", "consumers", "to", "shun", "British", "lamb", "until", "scientists", "determine", "whether", "mad", "cow", "disease", "can", "be", "transmitted", "to", "sheep", "."]
+}
+```
+
+PhoNER_COVID19 has a `tokens` and a `ner_tags` field, which is the format expected by the script.
+I simply loaded the JSON files and pushed them to the huggingface hub, which converts them into the parquest format.
+
+### Text Classification
+For this thesis I have included a dataset for hate speech detection in Vietnamese (ViHSD) and a natural language 
+inference dataset (XNLI).
+
+For ViHSD, you can use the `run_text_classification.py` script::
+```bash
+python src/training/run_classification.py \
+    --model_name_or_path FacebookAI/xlm-roberta-base \
+    --dataset_name phucdev/ViHSD \
+    --cache_dir /vol/tmp/truongph \
+    --shuffle_train_dataset \
+    --metric_name f1 \
+    --text_column_name free_text \
+    --label_column_name label_id \
+    --do_train \
+    --do_eval \
+    --max_seq_length 512 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 1 \
+    --seed 42 \
+    --output_dir outputs/ViHSD/
+```
+
+For XNLI, you can use the `run_xnli.py` script:
+```bash
+python src/training/run_xnli.py \
+  --model_name_or_path FacebookAI/xlm-roberta-base \
+  --language vi \
+  --train_language vi \
+  --do_train \
+  --do_eval \
+  --per_device_train_batch_size 8 \
+  --learning_rate 5e-5 \
+  --num_train_epochs 2.0 \
+  --max_seq_length 128 \
+  --output_dir outputs/xnli \
+  --save_steps -1
+```
+This should run 106 mins on a single tesla V100 16GB.
+Training with the previously defined hyperparameters yields the following results on the test set:
+```
+acc = 0.7093812375249501
+```
+
+### Question Answering
+For Question Answering, you can use the `run_qa.py` script:
+```bash
+python src/training/run_qa.py \
+  --model_name_or_path=FacebookAI/xlm-roberta-base \
+  --dataset_name=phucdev/ViMLQA \
+  --output_dir=outputs/mlqa \
+  --do_train \
+  --do_eval \
+  --per_device_train_batch_size 4 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --seed=42
+```
+
+Note that if your dataset contains samples with no possible answers (like SQuAD version 2), you need to pass along the 
+flag `--version_2_with_negative`.
+Your dataset should be structured like SQuAD to ensure compatibility with the script.
---
+++ src/training/train_tokenizer.py
@@ -0,0 +1,216 @@
+import datasets
+import logging
+import argparse
+import requests
+import time
+import numpy as np
+
+from pathlib import Path
+
+from datasets import IterableDataset
+from tokenizers import normalizers
+from transformers import AutoTokenizer
+from rich.logging import RichHandler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(RichHandler())
+logger.setLevel(logging.INFO)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train a tokenizer")
+    parser.add_argument(
+        "--dataset_name_or_path",
+        type=str,
+        help="Name of the dataset to train the tokenizer on",
+    )
+    parser.add_argument(
+        "--dataset_config_name",
+        type=str,
+        help="Name of the dataset config to use",
+    )
+    parser.add_argument(
+        "--cache_dir",
+        type=str,
+        default=None,
+        help="Directory to cache the dataset",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        help="Output directory to save the tokenizer",
+        required=True,
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=1000,
+        help="Batch size to use for training the tokenizer",
+    )
+    parser.add_argument(
+        "--vocab_size",
+        type=int,
+        default=50000,
+        help="Size of the vocabulary to use",
+    )
+    parser.add_argument(
+        "--max_num_space_separated_tokens",
+        type=int,
+        default=None,
+        help="Filter out long texts from the dataset (per default: > 32K space separated tokens)",
+    )
+    parser.add_argument(
+        "--max_num_bytes",
+        type=int,
+        default=None,
+        help="Filter out long texts from the dataset (length measured in bytes)",
+    )
+    parser.add_argument(
+        "--num_proc",
+        type=int,
+        default=4,
+        help="Number of processes to use when filtering out long texts from the dataset",
+    )
+    parser.add_argument(
+        "--original_tokenizer",
+        type=str,
+        help="Name of the original tokenizer to use in order to use the same tokenization algorithm and special tokens",
+        required=True,
+    )
+    arguments = parser.parse_args()
+    return arguments
+
+
+def streaming_batch_iterator(
+        data: IterableDataset,
+        batch_size: int = 1000,
+        max_retries: int = 3,
+        retry_delay: int = 15,
+        max_num_space_separated_tokens: int = None,
+        max_num_bytes: int = None
+):
+    # This function is used to iterate over the dataset when streaming
+    batch = []
+    retries = 0
+    state_dict = None
+
+    while retries <= max_retries:
+        try:
+            if state_dict:
+                data.load_state_dict(state_dict)
+            for idx, example in enumerate(data):
+                if (max_num_space_separated_tokens and
+                        0 < max_num_space_separated_tokens < len(example["text"].split())):
+                    continue
+                elif max_num_bytes and sentence_length_in_bytes(example["text"]) > max_num_bytes:
+                    continue
+                batch.append(example["text"])
+                state_dict = data.state_dict()
+                if len(batch) == batch_size:
+                    yield batch
+                    batch = []
+            break  # Exit the loop if successful
+        except requests.exceptions.ConnectionError:
+            retries += 1
+            if retries > max_retries:
+                logger.error(f"Failed to connect to the server after {max_retries} retries.")
+                raise
+            logger.warning(f"Connection error. Retrying in {retry_delay}s [{retries}/{max_retries}]")
+            time.sleep(retry_delay)
+    if batch:  # yield last batch
+        yield batch
+
+
+def batch_iterator(data, batch_size=1000):
+    for start_idx in range(0, len(data), batch_size):
+        samples = data[start_idx:start_idx + batch_size]
+        yield samples["text"]
+
+
+def sentence_length_in_bytes(sentence):
+    # Encode the sentence into bytes using UTF-8 encoding
+    byte_representation = sentence.encode('utf-8')
+    # Return the length of the byte array
+    return len(byte_representation)
+
+
+def get_num_words(data):
+    def compute_length(example):
+        return {"num_words": len(example["text"].split())}
+
+    data_with_lengths = data.map(compute_length)
+    num_words = np.array(data_with_lengths["num_words"])
+    num_words.sort()
+    return list(num_words)
+
+
+def main():
+    args = parse_args()
+    logger.info(f"Training tokenizer on {args.dataset_name_or_path} with vocab size {args.vocab_size}")
+    # Local dataset
+    if Path(args.dataset_name_or_path).exists():
+        logger.info(f"Loading dataset from local file: {args.dataset_name_or_path}")
+        data = datasets.load_dataset("json", data_files=args.dataset_name_or_path, split="train")
+        original_size = len(data)
+        if args.max_num_space_separated_tokens and args.max_num_space_separated_tokens > 0:
+            data = data.filter(lambda x: len(x["text"].split()) <= args.max_num_space_separated_tokens,
+                               num_proc=args.num_proc)
+        elif args.max_num_bytes and args.max_num_bytes > 0:
+            data = data.filter(lambda x: sentence_length_in_bytes(x["text"]) <= args.max_num_bytes,
+                               num_proc=args.num_proc)
+        logger.info(f"Filtered out long texts with >{args.max_num_space_separated_tokens} space separated tokens. "
+                    f"Original dataset size: {original_size}. New dataset size: {len(data)}")
+        training_corpus = batch_iterator(data)
+    else:
+        # Load dataset from the Hugging Face hub
+        if args.cache_dir:
+            logger.info(f"Loading {args.dataset_name_or_path} dataset from the Hugging Face hub")
+            data = datasets.load_dataset(
+                args.dataset_name_or_path,
+                args.dataset_config_name,
+                split="train",
+                cache_dir=args.cache_dir
+            )
+            original_size = len(data)
+            if args.max_num_space_separated_tokens and args.max_num_space_separated_tokens > 0:
+                data = data.filter(lambda x: len(x["text"].split()) <= args.max_num_space_separated_tokens,
+                                   num_proc=args.num_proc)
+                logger.info(
+                    f"Filtered out long texts with >{args.max_num_space_separated_tokens} space separated tokens. "
+                    f"Original dataset size: {original_size}. New dataset size: {len(data)}")
+            elif args.max_num_bytes and args.max_num_bytes > 0:
+                data = data.filter(lambda x: sentence_length_in_bytes(x["text"]) <= args.max_num_bytes,
+                                   num_proc=args.num_proc)
+                logger.info(
+                    f"Filtered out long texts with >{args.max_num_bytes} bytes. "
+                    f"Original dataset size: {original_size}. New dataset size: {len(data)}")
+
+            training_corpus = batch_iterator(data, batch_size=args.batch_size)
+        else:
+            # Stream the dataset (default)
+            logger.info(f"Streaming {args.dataset_name_or_path} from the Hugging Face hub")
+            data = datasets.load_dataset(
+                args.dataset_name_or_path, args.dataset_config_name, split="train", streaming=True
+            )
+            training_corpus = streaming_batch_iterator(
+                data,
+                batch_size=args.batch_size,
+                max_num_space_separated_tokens=args.max_num_space_separated_tokens,
+                max_num_bytes=args.max_num_bytes,
+            )
+
+    old_tokenizer = AutoTokenizer.from_pretrained(args.original_tokenizer)
+    assert old_tokenizer.is_fast, "This script only works with fast tokenizers"
+    logger.info(f"Training new tokenizer based on {args.original_tokenizer}")
+    old_tokenizer.normalizer = normalizers.NFKC()
+    tokenizer = old_tokenizer.train_new_from_iterator(
+        training_corpus,
+        vocab_size=args.vocab_size,
+        show_progress=True,
+    )
+    logger.info(f"Saving tokenizer to {args.output_dir}")
+    tokenizer.save_pretrained(args.output_dir)
+
+
+if __name__ == "__main__":
+    main()

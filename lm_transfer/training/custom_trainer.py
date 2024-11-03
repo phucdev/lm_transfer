054849944addae3403ea6b489144816a0726f93c
Phuc Tran Truong <ptruong@outlook.de>
2024-11-03 22:59:08 +0100

    Add custom_trainer.py with option to use weighted loss (for imbalanced datasets)

---
+++ lm_transfer/training/custom_trainer.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import Trainer
+
+
+class CustomTrainer(Trainer):
+    def __init__(self, *args, class_weights=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.class_weights = class_weights
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        # Forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+
+        # Use weighted cross-entropy loss
+        if self.class_weights is not None:
+            loss_fct = torch.nn.CrossEntropyLoss(weight=self.class_weights.to(logits.device))
+        else:
+            loss_fct = torch.nn.CrossEntropyLoss()
+
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+
+        return (loss, outputs) if return_outputs else loss

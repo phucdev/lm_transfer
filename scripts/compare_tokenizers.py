c76d667a25d6d1801cf2029d2c14edd6fc10dbc9
Phuc Tran Truong <ptruong@outlook.de>
2025-02-02 18:13:40 +0100

    PLot vocabulary overlap in one figure for monolingual and multilingual scenario

--- scripts/compare_tokenizers.py
+++ scripts/compare_tokenizers.py
@@ -13,90 +13,79 @@ logger = logging.getLogger(__name__)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src_model", type=str, default=None, help="The model_name_or_path of the source model")
-    parser.add_argument("--tgt_model", type=str, default=None,help="The model_name_or_path of the target model")
-    parser.add_argument("--language", type=str, default=None, choices=["monolingual", "multilingual"],
-                        help="The language of the tokenizer (monolingual or multilingual)")
     parser.add_argument("--output_path", type=str, default=None, help="The path to save the plot")
     parser.add_argument("--title", type=str, default=None, help="The title of the plot")
+    parser.add_argument("--include_canonicalized", action="store_true", default=False, help="Include overlap of canonicalized tokens in the plot")
+    parser.add_argument("--include_fuzzy", action="store_true", default=False, help="Include overlap of fuzzy matched tokens in the plot")
+    parser.add_argument("--fig_size", type=tuple, default=(7, 5), help="The size of the plot")
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
-    if args.src_model and args.tgt_model:
-        src_model = args.src_model
-        tgt_model = args.tgt_model
-    elif args.language == "monolingual":
-        src_model = "FacebookAI/roberta-base"
-        tgt_model = "phucdev/vi-bpe-culturax-2048"
-    elif args.language == "multilingual":
-        src_model = "FacebookAI/xlm-roberta-base"
-        tgt_model = "phucdev/vi-spm-culturax-2048"
-    else:
-        raise ValueError("Please provide either src_model and tgt_model or language")
-    src_tokenizer = AutoTokenizer.from_pretrained(src_model)
-    tgt_tokenizer = AutoTokenizer.from_pretrained(tgt_model)
-
-    # Simple overlap analysis -> CLP method
-    target_tokens = set(tgt_tokenizer.get_vocab().keys())
-    source_tokens = set(src_tokenizer.get_vocab().keys())
-
+def get_exact_overlap(target_tokenizer, source_tokenizer):
+    target_tokens = set(target_tokenizer.get_vocab().keys())
+    source_tokens = set(source_tokenizer.get_vocab().keys())
     exact_overlapping_tokens = target_tokens & source_tokens
     exact_missing_tokens = target_tokens - source_tokens
+    return exact_overlapping_tokens, exact_missing_tokens
 
-    logger.info(f'Simple match (CLP): {len(exact_overlapping_tokens)=}; {len(exact_missing_tokens)=}')
 
-
-    # FOCUS method of overlap analysis involves canonicalization
+def get_canonicalized_overlap(target_tokenizer, source_tokenizer):
     canon_overlapping_tokens, canon_missing_tokens = get_overlapping_tokens(
-        target_tokenizer=tgt_tokenizer,
-        source_tokenizer=src_tokenizer,
+        target_tokenizer=target_tokenizer,
+        source_tokenizer=source_tokenizer,
         exact_match_all=True,
         match_symbols=False,
         fuzzy_match_all=False
     )
-    # Sort to ensure same order every time (especially important when executing on multiple ranks)
-    # Target token -> source token(s)
-    canon_overlapping_tokens = sorted(canon_overlapping_tokens.items(), key=lambda x: x[1].target.id)
-    canon_missing_tokens = sorted(canon_missing_tokens.items(), key=lambda x: x[1].target.id)
+    return canon_overlapping_tokens, canon_missing_tokens
 
-    logger.info(f'Canonicalization (FOCUS): {len(canon_overlapping_tokens)=}; {len(canon_missing_tokens)=}')
 
-    # FOCUS method of overlap analysis: fuzzy matching ignoring whitespace and case
+def get_fuzzy_overlap(target_tokenizer, source_tokenizer):
     fuzzy_overlapping_tokens, fuzzy_missing_tokens = get_overlapping_tokens(
-        target_tokenizer=tgt_tokenizer,
-        source_tokenizer=src_tokenizer,
+        target_tokenizer=target_tokenizer,
+        source_tokenizer=source_tokenizer,
         exact_match_all=True,
         match_symbols=False,
         fuzzy_match_all=True
     )
-    # Sort to ensure same order every time (especially important when executing on multiple ranks)
-    # Target token -> source token(s)
-    fuzzy_overlapping_tokens = sorted(fuzzy_overlapping_tokens.items(), key=lambda x: x[1].target.id)
-    fuzzy_missing_tokens = sorted(fuzzy_missing_tokens.items(), key=lambda x: x[1].target.id)
+    return fuzzy_overlapping_tokens, fuzzy_missing_tokens
 
-    logger.info(f'Fuzzy Matching (FOCUS): {len(fuzzy_overlapping_tokens)=}; {len(fuzzy_missing_tokens)=}')
 
-    # Now create a bar plot to compare the number of overlapping tokens and missing tokens using the three matching methods
+def main():
+    args = parse_args()
+    mono_src_model = "FacebookAI/roberta-base"
+    mono_tgt_model = "phucdev/vi-bpe-culturax-2048"
+    multi_src_model = "FacebookAI/xlm-roberta-base"
+    multi_tgt_model = "phucdev/vi-spm-culturax-2048"
+
+    mono_src_tokenizer = AutoTokenizer.from_pretrained(mono_src_model)
+    mono_tgt_tokenizer = AutoTokenizer.from_pretrained(mono_tgt_model)
+    multi_src_tokenizer = AutoTokenizer.from_pretrained(multi_src_model)
+    multi_tgt_tokenizer = AutoTokenizer.from_pretrained(multi_tgt_model)
+
+    # Calculate the overlap for each model pair and then plot the results
+    models = ["RoBERTa", "XLM-RoBERTa"]
+
+    # Get the exact overlap
+    mono_exact_overlap, mono_exact_missing = get_exact_overlap(mono_tgt_tokenizer, mono_src_tokenizer)
+    multi_exact_overlap, multi_exact_missing = get_exact_overlap(multi_tgt_tokenizer, multi_src_tokenizer)
+    overlap_counts = [len(mono_exact_overlap), len(multi_exact_overlap)]
+    missing_counts = [len(mono_exact_missing), len(multi_exact_missing)]
+
+    # Now create a bar plot to compare the number of overlapping tokens and missing tokens
     # Create a bar plot
-    # 1) Prepare data
-    methods = ["Exact", "Canonical", "Fuzzy"]
-    overlap_counts = [len(exact_overlapping_tokens), len(canon_overlapping_tokens), len(fuzzy_overlapping_tokens)]
-    missing_counts = [len(exact_missing_tokens), len(canon_missing_tokens), len(fuzzy_missing_tokens)]
-
-    x = np.arange(len(methods))
+    x = np.arange(len(models))
     width = 0.5
 
     # 2) Create the figure and axes
-    fig, ax = plt.subplots(figsize=(7, 5))
+    fig, ax = plt.subplots(figsize=args.fig_size)
 
     # 3) Plot stacked bars
     bar_overlap = ax.bar(
         x,
         overlap_counts,
         width,
-        label="Overlap",
+        label="Overlapping Tokens",
         color="#1f77b4"  # e.g. a nice blue
     )
     bar_missing = ax.bar(
@@ -104,18 +93,18 @@ def main():
         missing_counts,
         width,
         bottom=overlap_counts,
-        label="Missing",
+        label="Non-Overlapping Tokens",
         color="#ff7f0e"  # e.g. an orange
     )
 
     # 4) Customize
     ax.set_xticks(x)
-    ax.set_xticklabels(methods)
+    ax.set_xticklabels(models)
     ax.set_ylabel("Number of Tokens")
     if args.title:
         ax.set_title(args.title)
     else:
-        ax.set_title(f"Vocabulary Overlap for {src_model} and {tgt_model}")
+        ax.set_title(f"Vocabulary Overlap Between the Target and Source Tokenizers")
     ax.legend()
 
     # Now label the bars.

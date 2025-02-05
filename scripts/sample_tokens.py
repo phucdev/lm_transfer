80567591cc934d4efd3b970bc780298979d861e2
Phuc Tran Truong <ptruong@outlook.de>
2025-02-05 12:22:29 +0100

    Sort the methods for token entries

--- scripts/sample_tokens.py
+++ scripts/sample_tokens.py
@@ -29,6 +29,17 @@ METHOD_MAP = {
   "focus_multilingual_initialization": "FOCUS",
 }
 
+BPE_TOKEN_SAMPLE = {
+    "high": ['Ġgá»Ĺ', 'ĠcÆ°á»Ŀi', 'ĠÄĳá»«ng'],  # gỗ (wood), cười (to laugh), đừng (don't)
+    "medium": ['ĠÄĳiÃªn', 'ĠxÄĥng', 'Ġtre'],    # điên (crazy), xăng (gasoline), tre (bamboo)
+    "low": ['Ġxá»Ľi', 'ĠgÃłi', 'Ġpháº©y']    # xới (to stir), gài (to fasten), phẩy (to flick/ comma)
+}
+SPM_TOKEN_SAMPLE = {
+    "high": ['▁đỏ', '▁hành', '▁cũ'],    # đỏ (red), hành (onion/ to torture), cũ (old)
+    "medium": ['▁bám', '▁vầy', '▁núi'],   # bám (to cling), vầy (like this/ to swirl), núi (mountain)
+    "low": ['▁thòi', '▁câm', '▁nhện']   # thòi (to protrude), câm (mute), nhện (spider)
+}
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -44,7 +55,8 @@ def parse_args():
     parser.add_argument("--num_proc", type=int, default=None)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--dictionary_path", type=str, default=None)
-    parser.add_argument("--min_token_length", type=int, default=3)
+    parser.add_argument("--sample_new_tokens", action="store_true", default=False, help="Whether to sample new tokens instead of using the pre-sampled ones")
+    parser.add_argument("--show_plot", action="store_true", default=False, help="Whether to show the token frequency histogram")
     return parser.parse_args()
 
 
@@ -104,6 +116,60 @@ def get_vietnamese_word_list(file_path):
     return word_list
 
 
+def get_token_frequency(data, tokenizer, num_proc):
+    # Count frequency of tokens in the tokenizer vocabulary in the data
+    target_tokenizer_vocab = tokenizer.get_vocab()
+
+    def tokenize_fn(examples):
+        return tokenizer(examples["text"])
+
+    data = data.map(tokenize_fn, batched=True, num_proc=num_proc)
+    # Count frequencies
+    token_frequencies = Counter()
+    for example in tqdm(data, desc="Counting token frequencies"):
+        token_frequencies.update(example['input_ids'])
+    id_to_token = {v: k for k, v in target_tokenizer_vocab.items()}
+    token_frequencies = {id_to_token[token]: freq for token, freq in token_frequencies.items()}
+    return token_frequencies
+
+
+def filter_frequency(token_frequencies, target_tokenizer, dictionary_path):
+    # Filter out special tokens and punctuation
+    special_tokens = target_tokenizer.all_special_tokens
+    punctuation = [".", ",", "!", "?", ":", ";", "(", ")", "[", "]", "{", "}"]
+    filtered_token_frequencies = {token: freq for token, freq in token_frequencies.items() if
+                                  token not in special_tokens and token not in punctuation}
+
+    # Only keep Vietnamese tokens: use a list as a filter
+    vietnamese_word_list = " ".join(get_vietnamese_word_list(dictionary_path))
+    filtered = {}
+    for token, freq in filtered_token_frequencies.items():
+        decoded_token = target_tokenizer.decode(target_tokenizer.convert_tokens_to_ids(token)).lower()
+        if decoded_token in vietnamese_word_list:
+            filtered[token] = freq
+    return filtered
+
+
+def get_token_sample(token_frequencies, sample_size):
+    # Sort the token frequencies highest to lowest
+    sorted_token_freqs = sorted(token_frequencies.items(), key=lambda x: x[1], reverse=True)
+    # partition the tokenizer vocabulary into three buckets: high-freq (top 10%), mid-freq (next 40%),
+    # low-freq (remaining 50%)
+    high_freq = sorted_token_freqs[:int(len(sorted_token_freqs) * 0.1)]
+    mid_freq = sorted_token_freqs[int(len(sorted_token_freqs) * 0.1):int(len(sorted_token_freqs) * 0.5)]
+    low_freq = sorted_token_freqs[int(len(sorted_token_freqs) * 0.5):]
+
+    # Randomly sample 3 tokens from each bucket
+    high_sample = [tok[0] for tok in random.sample(high_freq, sample_size)]
+    mid_sample = [tok[0] for tok in random.sample(mid_freq, sample_size)]
+    low_sample = [tok[0] for tok in random.sample(low_freq, sample_size)]
+    return {
+        "high": high_sample,
+        "mid": mid_sample,
+        "low": low_sample
+    }
+
+
 def main():
     args = parse_args()
 
@@ -122,86 +188,49 @@ def main():
     # missing_tokens = target_tokens - source_tokens
 
     if Path(args.output_dir).joinpath("token_frequencies.json").exists():
+        logger.info(f"Loading token frequencies from: {Path(args.output_dir).joinpath('token_frequencies.json')}")
         with open(Path(args.output_dir).joinpath("token_frequencies.json"), "r") as f:
             token_frequencies = json.load(f)
     else:
-        # Count frequency of tokens in the tokenizer vocabulary in the data
-        target_tokenizer_vocab = target_tokenizer.get_vocab()
-        def tokenize_fn(examples):
-            return target_tokenizer(examples["text"])
-
-        data = data.map(tokenize_fn, batched=True, num_proc=args.num_proc)
-        # Count frequencies
-        token_frequencies = Counter()
-        for example in tqdm(data, desc="Counting token frequencies"):
-            token_frequencies.update(example['input_ids'])
-        id_to_token = {v: k for k, v in target_tokenizer_vocab.items()}
-        token_frequencies = {id_to_token[token]: freq for token, freq in token_frequencies.items()}
+        logger.info(f"Calculating token frequencies on {args.data_path=} using {args.target_tokenizer=}")
+        token_frequencies = get_token_frequency(data, target_tokenizer, args.num_proc)
         with open(Path(args.output_dir).joinpath("token_frequencies.json"), "w") as f:
             f.write(json.dumps(token_frequencies))
 
-    # Plot the token frequencies with a histogram
-    fig, ax = plt.subplots(figsize=(10, 6))
-    plt.hist(list(token_frequencies.values()), bins=100)
-    ax.set_title("Token Frequencies")
-    plt.tight_layout()
-    plt.show()
+    if args.show_plot:
+        # Plot the token frequencies with a histogram
+        fig, ax = plt.subplots(figsize=(10, 6))
+        plt.hist(list(token_frequencies.values()), bins=100)
+        ax.set_title("Token Frequencies")
+        plt.tight_layout()
+        plt.show()
 
-    # Filter out special tokens and punctuation
-    special_tokens = target_tokenizer.all_special_tokens
-    punctuation = [".", ",", "!", "?", ":", ";", "(", ")", "[", "]", "{", "}"]
-    filtered_token_frequencies = {token: freq for token, freq in token_frequencies.items() if
-                                    token not in special_tokens and token not in punctuation}
+    filtered = filter_frequency(token_frequencies, target_tokenizer, args.dictionary_path)
 
-    # Only keep Vietnamese tokens: use a list as a filter
-    vietnamese_word_list = " ".join(get_vietnamese_word_list(args.dictionary_path))
-    filtered = {}
-    for token, freq in filtered_token_frequencies.items():
-        decoded_token = target_tokenizer.decode(target_tokenizer.convert_tokens_to_ids(token)).lower()
-        if decoded_token in vietnamese_word_list and len(decoded_token) >= args.min_token_length:
-            filtered[token] = freq
-
-    # Sort the token frequencies highest to lowest
-    sorted_token_freqs = sorted(filtered_token_frequencies.items(), key=lambda x: x[1], reverse=True)
-    # partition the tokenizer vocabulary into three buckets: high-freq (top 10%), mid-freq (next 40%),
-    # low-freq (remaining 50%)
-    high_freq = sorted_token_freqs[:int(len(sorted_token_freqs) * 0.1)]
-    mid_freq = sorted_token_freqs[int(len(sorted_token_freqs) * 0.1):int(len(sorted_token_freqs) * 0.5)]
-    low_freq = sorted_token_freqs[int(len(sorted_token_freqs) * 0.5):]
-
-    # Randomly sample 3 tokens from each bucket
-    high_sample = random.sample(high_freq, 3)
-    mid_sample = random.sample(mid_freq, 3)
-    low_sample = random.sample(low_freq, 3)
-    # If we want to include at least one overlapping token per bucket we can use the sample_from_bucket function
+    logger.info("Sampling tokens for each frequency bucket")
+    if args.sample_new_tokens:
+        token_sample = get_token_sample(filtered, args.num_tokens)
+    elif args.target_tokenizer == "phucdev/vi-bpe-culturax-4g-sample":
+        token_sample = BPE_TOKEN_SAMPLE
+    elif args.target_tokenizer == "phucdev/vi-spm-culturax-4g-sample":
+        token_sample = SPM_TOKEN_SAMPLE
+    else:
+        token_sample = get_token_sample(filtered, args.num_tokens)
 
     # Then go through each model directory containing a sources.json file
     # Create a table that shows the token, its frequency bucket, the method of its embedding initialization,
     # and the top source tokens and their weights
     table = []
-    for token, freq in high_sample:
-        table.append({
-            "token": token,
-            "decoded_token": target_tokenizer.decode(target_tokenizer.convert_tokens_to_ids(token)),
-            "frequency_bucket": "high",
-            "sources": {}
-        })
-    for token, freq in mid_sample:
-        table.append({
-            "token": token,
-            "decoded_token": target_tokenizer.decode(target_tokenizer.convert_tokens_to_ids(token)),
-            "frequency_bucket": "mid",
-            "sources": {}
-        })
-    for token, freq in low_sample:
-        table.append({
-            "token": token,
-            "decoded_token": target_tokenizer.decode(target_tokenizer.convert_tokens_to_ids(token)),
-            "frequency_bucket": "low",
-            "sources": {}
-        })
+    for bucket, tokens in token_sample.items():
+        for token in tokens:
+            table.append({
+                "token": token,
+                "decoded_token": target_tokenizer.decode(target_tokenizer.convert_tokens_to_ids(token)),
+                "frequency_bucket": bucket,
+                "sources": {}
+            })
 
-    for model_dir in tqdm(os.listdir(args.input_dir)):
+    for model_dir in tqdm(os.listdir(args.input_dir), desc="Processing model directories"):
         model_path = os.path.join(args.input_dir, model_dir)
         if not os.path.isdir(model_path):
             continue
@@ -236,6 +265,12 @@ def main():
                     "top_source_tokens": [],
                     "num_source_tokens": 0
                 }
+
+    # Order sources methods by the order in the METHOD_MAP
+    for entry in table:
+        entry_sources = [(k, v) for k, v in entry["sources"].items()]
+        entry_sources.sort(key=lambda x: list(METHOD_MAP.values()).index(x[0]))
+        entry["sources"] = {k: v for k, v in entry_sources}
 
     with open(os.path.join(args.output_dir, "sample_tokens.json"), "w") as f:
         f.write(json.dumps(table, indent=2, ensure_ascii=False))
